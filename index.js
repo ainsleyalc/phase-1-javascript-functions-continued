e3df210bc2dc2b9be50d2edc1ffd5071d3e95e4e
Ainsley Alceme <youngchill101@gmail.com>
2023-03-23 15:56:41 -0400

    submits

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+
+
+function saturdayFun(name = "roller-skate"){
+    return `This Saturday, I want to ${name}!`
+}
+saturdayFun("bathe my dog")
+
+
+function mondayWork (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.` 
+}
+mondayWork('work from home')
+
+
+function wrapAdjective(ppp = '!') {
+    return  function (def){
+        return `You are ${ppp}${def}${ppp}!`
+    }
+
+      }
+      wrapAdjective('*')("a dedicated programmer")
+
+
+
+
+
+//=> "You are %a dedicated programmer%!"
+
